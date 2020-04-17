abc71d9171ae434ca5b454778d7aebcd2dfaac5b
superce <1594007655@qq.com>
2020-04-17 17:55:39 +0800

    删,改,查

---
+++ mysql/index.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'benji',
+  port:'3306'
+})
+
+let query = function( sql, values ) {
+  return new Promise(( resolve, reject ) => {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        reject( err )
+      } else {
+        connection.query(sql, values, ( err, rows) => {
+
+          if ( err ) {
+            reject( err )
+          } else {
+            resolve( rows )
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+}
+
+module.exports = { query }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,123 @@
 const route = require('koa-router')
+const { query } = require('../mysql/index')
 let router = new route()
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!',
-    content:'888888888888888'
+
+
+async function selectAllData() {
+  let sql = 'SELECT * FROM plat_shop_datas'
+  let dataList = await query(sql)
+  return dataList
+}
+
+async function deleteShop(id){
+  let sql = `DELETE FROM plat_shop_datas where id='${id}'`
+  let deleteList = await query(sql,id)
+  return deleteList
+}
+
+async function updata(params){
+  let id = params.id
+  let name = params.shop_name
+  let sql = `UPDATE plat_shop_datas SET shop_name = '${name}' WHERE id = '${id}' `
+  let update = await query(sql,params)
+  return update
+}
+
+router.get('/list', async (ctx, next) => {
+  let name = []
+  await selectAllData().then(res =>{
+    res.forEach(item =>{
+      if(item.type===1&&item.total_fee===0){
+        name.push(item)
+      }
+    })
+    ctx.body = {
+      code:200,
+      msg:'成功',
+      data:name  
+    }
+  }).catch(err =>{
+    ctx.body = {
+      code:200,
+      msg:err,
+      data:name  
+    }
   })
+})
+
+router.post('/delete_shop',async (ctx,next)=>{
+  let id= ctx.request.body;
+  let result = false
+  await selectAllData().then(res =>{
+    res.forEach(item =>{
+      if(id.id===item.id){
+        result = true
+      }
+    })
+  })
+  if(result){
+    await deleteShop(id.id).then(res =>{
+      ctx.body = {
+        code:200,
+        msg:'成功',
+        state:true
+      }
+    }).catch(err =>{
+      ctx.body={
+        code:500,
+        msg:'失败',
+        state:false
+      }
+    })
+  }else{
+    ctx.body={
+      code:200,
+      msg:'没有此数据',
+      state:false
+    }
+  }
+})
+
+router.post('/updata',async (ctx,next)=>{
+  let name = ctx.request.body
+  let result = false
+  if(!name.shop_name){
+    ctx.body = {
+      code:200,
+      msg:'空值',
+      state:false
+    }
+    return false
+  }
+  await selectAllData().then(res =>{
+    res.forEach(item =>{
+      if(name.shop_name===item.shop_name){
+        console.log(item.shop_name)
+        result = true
+      }
+    })
+  })
+  
+  if(result){
+    ctx.body = {
+      code:200,
+      msg:'重复',
+      state:false
+    }
+  }else{
+    await updata(name).then(res =>{
+      ctx.body = {
+        code:200,
+        msg:'成功',
+        state:true
+      }
+    }).catch(err =>{
+      ctx.body = {
+        code:500,
+        msg:'错误'
+      }
+    })
+  }
 })
 
 router.get('/string', async (ctx, next) => {
@@ -12,6 +125,7 @@ router.get('/string', async (ctx, next) => {
 })
 
 router.get('/json', async (ctx, next) => {
+  // await ctx.render()
   ctx.body = {
     title: 'koa2 json',
     li:'546'
@@ -19,3 +133,4 @@ router.get('/json', async (ctx, next) => {
 })
 
 module.exports = router
+
